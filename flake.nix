b4314bd61892cc851339059ac06359d50201bbec
teamie <79058304+tea-scripts@users.noreply.github.com>
2024-09-27 20:25:46 +0800

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        ## unfree packages you want to enable
+        # config.allowUnfreePredicate = pkg:
+        #   builtins.elem (nixpkgs.lib.getName pkg) [ ];
+      };
+    in {
+      devShells.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            ## Optional name for the shell
+            # name = "My pearly shell";
+            languages = {
+              nix.enable = true;
+              ## Add more languages depending on your project
+              python.enable = true;
+              typescript.enable = true;
+              go.enable = true;
+            };
+            ## Add more packages required in the shell
+            packages = with pkgs; [ exercism python312Packages.pip python312Packages.pytest yarn typescript corepack ];
+          }
+        ];
+      };
+    });
+}
